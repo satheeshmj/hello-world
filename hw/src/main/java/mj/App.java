9befa1fa8e474152d41985ba4ea37e316a7dee2f
satheeshmj <satheeshmj@users.noreply.github.com>
2023-05-26 15:34:10 +0530

    Test sqc (#4)

--- hw/src/main/java/mj/App.java
+++ hw/src/main/java/mj/App.java
@@ -6,8 +6,9 @@ package mj;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        // test out sqc
+        // for c2
         System.out.println( "Hello World!" );
     }
 }
